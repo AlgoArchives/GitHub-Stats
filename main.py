320e23c0ae4709515a9754638c6993555fdf1d31
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-06-08 18:49:07 +0530

    Fixed os dir path

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fetch_contributions import fetch_github_events, calculate_streaks
 from generate_svg import generate_svg
+import os
 
 if __name__ == '__main__':
     username = 'Vikranth3140'
+
+    # Ensure the output directory exists
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
     events = fetch_github_events(username)
     total_contributions, current_streak, longest_streak = calculate_streaks(events)
     generate_svg(total_contributions, current_streak, longest_streak)
